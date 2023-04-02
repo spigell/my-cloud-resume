d65c29ef8ea232d89a8373757f0019d61e204d45
Spigell <spigelly@gmail.com>
2023-04-02 17:14:05 +0400

    :wrench: deploy: temporary allow all for cloud the run service

--- infra/gcp/gcs-proxy.ts
+++ infra/gcp/gcs-proxy.ts
@@ -11,7 +11,7 @@ export function Deploy(project: string, region: string, image: string) {
       project: project,
       metadata: {
         annotations: {
-          'run.googleapis.com/ingress': 'internal-and-cloud-load-balancing',
+          'run.googleapis.com/ingress': 'All',
         },
       },
       template: {
