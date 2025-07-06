3a10c92a7c9a18e646e70685e4f2c5ab85c08fd0
spigell <Spigelly@gmail.com>
2025-07-06 13:42:04 +0300

    add a new pdf (#19)

---
+++ resume/src/pdf/devops-sre-pdf-en.ts
@@ -0,0 +1,43 @@
+import * as en from '../data/common/sre-devops-en';
+
+const version = 'v0.0.1';
+
+const jsonData = JSON.stringify(
+  {
+    meta: {
+      version: version,
+      'x-lang': 'en',
+    },
+    basics: en.data.basics,
+    work: [
+      en.data.work.freelance,
+      en.data.work.amarkets,
+      {
+        ...en.data.work.gaijin,
+        summary: en.data.work.gaijin.summary.slice(0, 257),
+      },
+      en.data.work.yandex,
+      {
+        ...en.data.work.rostelecom,
+        summary: en.data.work.rostelecom.summary.slice(0, 261),
+      },
+      // ru.data.work.restream,
+    ],
+    skills: en.data.skills.filter((skill) => {
+      return [
+        'Kubernetes',
+        'IaC',
+        'Linux',
+        'Cloud',
+        'Databases',
+        'CI/CD',
+      ].includes(skill.name);
+    }),
+    certificates: [en.data.certificates.cka, en.data.certificates.cks],
+    languages: [en.data.languages.russian, en.data.languages.english],
+  },
+  null,
+  2,
+);
+
+console.log(jsonData);
